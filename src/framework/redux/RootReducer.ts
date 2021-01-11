51981c91dfcb899f4428b32f8eb72b60e37c7fd4
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2021-01-11 23:01:40 +1300

    Added redux-form

--- src/framework/redux/RootReducer.ts
+++ src/framework/redux/RootReducer.ts
@@ -1,7 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import { reducer as formReducer } from 'redux-form';
 import { connectRouter } from 'connected-react-router';
 import { History } from 'history';
 
-const getRootReducer = (history: History) => combineReducers({ router: connectRouter(history) });
+const getRootReducer = (history: History) =>
+  combineReducers({
+    router: connectRouter(history),
+    form: formReducer,
+  });
 
 export default getRootReducer;
