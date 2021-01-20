fc64b8220695d0295df765b0260ccef00a1a1852
Morteza Azizi <morteza@homiesalarm.nl>
2021-01-20 01:51:20 +0100

    Auth0 Integration

---
+++ src/views/home.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+import HomeContent from '../components/home-content';
+
+const Home = () => (
+  <Fragment>
+    <hr />
+    <HomeContent />
+  </Fragment>
+);
+
+export default Home;
