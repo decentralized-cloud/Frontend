7963ae4dd9712b8e4df67cc740f1b96972916013
Morteza <mori.azizi@gmail.com>
2021-01-20 02:20:18 +0100

    Auth0 Integration (#32)
    Co-authored-by: Morteza Azizi <morteza@homiesalarm.nl>

---
+++ src/components/nav-bar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AuthNav from './auth-nav';
+import MainNav from './main-nav';
+
+const NavBar = () => {
+  return (
+    <div className="nav-container mb-3">
+      <nav className="navbar navbar-expand-md navbar-light bg-light">
+        <div className="container">
+          <div className="navbar-brand logo" />
+          <MainNav />
+          <AuthNav />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
