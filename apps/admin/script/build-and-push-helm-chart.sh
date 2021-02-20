fad35a4c090f12a2525ccfb80d553d1a2be2a810
Chase Manning <me@chasemanning.co.nz>
2021-02-20 15:19:42 +1300

    :construction_worker: fix docker paths

--- apps/admin/script/build-and-push-helm-chart.sh
+++ apps/admin/script/build-and-push-helm-chart.sh
@@ -11,5 +11,5 @@ fi
 
 cd "$current_directory"/..
 
-docker build --build-arg GITHUB_ACCESS_CREDENTIAL=$GITHUB_ACCESS_CREDENTIAL -f docker/Dockerfile.buildAndPushHelmChart .
+docker build --build-arg GITHUB_ACCESS_CREDENTIAL=$GITHUB_ACCESS_CREDENTIAL -f apps/admindocker/Dockerfile.buildAndPushHelmChart .
 
