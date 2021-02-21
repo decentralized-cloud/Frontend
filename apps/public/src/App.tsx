b7b172290e0180cbade20052babbb9c493a3a4f3
Chase Manning <me@chasemanning.co.nz>
2021-02-21 14:12:52 +1300

    :sparkles: add react router

--- apps/public/src/App.tsx
+++ apps/public/src/App.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import styled from "styled-components";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import HomePage from "./components/pages/home/HomePage";
 
 const StyledApp = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
 `;
 
 const App = () => {
-  return <StyledApp>meow</StyledApp>;
+  return (
+    <StyledApp>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
+    </StyledApp>
+  );
 };
 
 export default App;
