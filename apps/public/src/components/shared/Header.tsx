91b3d61386ca0dc9b8e4d3afe811240ddf470f6f
Chase Manning <me@chasemanning.co.nz>
2021-02-21 14:12:59 +1300

    :sparkles: create header

---
+++ apps/public/src/components/shared/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledHeader = styled.div`
+  width: 100%;
+  height: 10rem;
+`;
+
+const Header = () => {
+  return <StyledHeader></StyledHeader>;
+};
+
+export default Header;
