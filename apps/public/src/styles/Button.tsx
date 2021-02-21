16eb2cb97d3135bf1c04ba47e45ca33b81a18f94
Chase Manning <me@chasemanning.co.nz>
2021-02-21 15:56:24 +1300

    :sparkles: add button mega mode

--- apps/public/src/styles/Button.tsx
+++ apps/public/src/styles/Button.tsx
@@ -2,10 +2,11 @@ import styled from "styled-components";
 
 type Props = {
   primary?: boolean;
+  mega?: boolean;
 };
 
 const Button = styled.button`
-  padding: 1.1rem 1.8rem;
+  padding: ${(props: Props) => (props.mega ? "1.6rem 4rem" : "1.1rem 1.8rem")};
   border: var(--primary);
   border: 1px solid var(--primary);
   background-color: ${(props: Props) =>
@@ -13,8 +14,10 @@ const Button = styled.button`
   color: ${(props: Props) =>
     props.primary ? "var(--light)" : "var(--primary)"};
   border-radius: 0.5rem;
-  font-size: 1.1rem;
+  font-size: ${(props: Props) => (props.mega ? "1.8rem" : "1.1rem")};
   text-transform: uppercase;
+  font-weight: ${(props: Props) => (props.mega ? "500" : "500")};
+  box-shadow: ${(props: Props) => (props.mega ? "var(--glow)" : "none")};
 `;
 
 export default Button;
