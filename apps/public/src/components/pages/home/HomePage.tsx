7557f2e17d6961a82f3f4d21d0bea57c627e79b9
Chase Manning <me@chasemanning.co.nz>
2021-02-21 14:12:38 +1300

    :sparkles: create home page

---
+++ apps/public/src/components/pages/home/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyleHomePage = styled.div`
+  width: 100%;
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const HomePage = () => {
+  return <StyleHomePage>meow</StyleHomePage>;
+};
+
+export default HomePage;
