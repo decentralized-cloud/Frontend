e5dc9abc7b01e5d54ba516f41b9c7ecfedb4c3dc
Chase Manning <me@chasemanning.co.nz>
2021-02-21 14:31:44 +1300

    :art: change landing to use css var

--- apps/public/src/components/pages/home/Landing.tsx
+++ apps/public/src/components/pages/home/Landing.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const StyledLanding = styled.div`
   width: 100%;
   height: 100vh;
-  background: linear-gradient(-125deg, #517ee7, #225add);
+  background: linear-gradient(-125deg, var(--light), var(--dark));
 `;
 
 const Landing = () => {
