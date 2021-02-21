e97d1b98760c62ceb4c29f973e3202a2523311af
Chase Manning <me@chasemanning.co.nz>
2021-02-21 14:23:40 +1300

    :art: make header position absolute

--- apps/public/src/components/shared/Header.tsx
+++ apps/public/src/components/shared/Header.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledHeader = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 10rem;
 `;
 
 const Header = () => {
-  return <StyledHeader></StyledHeader>;
+  return <StyledHeader>meow</StyledHeader>;
 };
 
 export default Header;
