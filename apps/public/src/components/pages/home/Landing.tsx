4f8295526ab8d72334b1588976c2d6967fe0a40c
Chase Manning <me@chasemanning.co.nz>
2021-02-21 14:23:49 +1300

    :sparkles: create landing page

---
+++ apps/public/src/components/pages/home/Landing.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledLanding = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(-125deg, #517ee7, #225add);
+`;
+
+const Landing = () => {
+  return <StyledLanding></StyledLanding>;
+};
+
+export default Landing;
