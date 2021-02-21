2220012d03b8b84ad3e42426851be3272234e2ea
Chase Manning <me@chasemanning.co.nz>
2021-02-21 13:51:27 +1300

    :sparkles: add styled components

--- apps/public/src/App.tsx
+++ apps/public/src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 
-function App() {
-  return <p>meow</p>;
-}
+const StyledApp = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const App = () => {
+  return <StyledApp>meow</StyledApp>;
+};
 
 export default App;
